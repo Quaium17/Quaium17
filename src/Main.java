8b3b33973adb6c8ef3de975beb1ab1046915bdfe
Quaium17 <s29894@pjwstk.edu.pl>
2024-10-10 16:34:35 +0200

    !

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder ();
         System.out.println(adder.add(1, 2));
-
         Subtractor suctractor = new Subtractor ();
         System.out.println(subtractor . subtract(6, 3));
 
